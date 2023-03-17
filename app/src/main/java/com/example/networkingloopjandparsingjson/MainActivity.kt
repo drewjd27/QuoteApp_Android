c3e7d6b85ee5c318f201569bbe0f5cf745f87dc9
Andrew Jonatan Damanik <andrewdamanik23@gmail.com>
2023-03-17 19:26:39 +0700

    creating getRandomQuote method in MainActivity

--- app/src/main/java/com/example/networkingloopjandparsingjson/MainActivity.kt
+++ app/src/main/java/com/example/networkingloopjandparsingjson/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.networkingloopjandparsingjson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.networkingloopjandparsingjson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getRandomQuote()
     }
+
+    private getRandomQuote() {
+
+    }
+
 }
